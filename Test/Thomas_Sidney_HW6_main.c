68b187ba675dce4a374a5374d83201edbcbc3eb5
Sidney <sid.14boys@yahoo.fr>
2023-12-08 14:44:31 -0800

    Add Igpay Atinlay set up + buffer variable

--- Test/Thomas_Sidney_HW6_main.c
+++ Test/Thomas_Sidney_HW6_main.c
@@ -6,8 +6,17 @@
 #include <unistd.h>
 #include <errno.h>
 
+char write_buffer[1024];       // buffer to store user input for writing to the device
+char read_buffer[1024];        // buffer to store data read from the device
+char translation_buffer[1024]; // buffer to hold translated text
+
 int main(int argc, char *argv[])
 {
-    printf("Welcome to the goku Test Program\n");
-    return 0;
+    int device_file, info;
+    int language_switch = 0;
+    char switcher[8];
+    char user_option[8];
+    printf("---------------------------------------------------\n");
+    printf(" - - -  Welcome to Igpay Atinlay Translator - - -   \n");
+    printf("---------------------------------------------------\n");
 }
