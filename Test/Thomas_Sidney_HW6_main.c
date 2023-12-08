7d9bf62e2735ed980a0b80e41b365aa823f53423
Sidney <sid.14boys@yahoo.fr>
2023-12-08 14:48:28 -0800

    Add language translation options and device file handling

--- Test/Thomas_Sidney_HW6_main.c
+++ Test/Thomas_Sidney_HW6_main.c
@@ -19,4 +19,19 @@ int main(int argc, char *argv[])
     printf("---------------------------------------------------\n");
     printf(" - - -  Welcome to Igpay Atinlay Translator - - -   \n");
     printf("---------------------------------------------------\n");
+
+    // open the device file for read/write access
+    device_file = open("/dev/DeviceDriver", O_RDWR);
+    if (device_file < 0)
+    {
+        printf("Cannot open device file...\n");
+        return 0;
+    }
+
+    // use ioctl to switch between languages (ENG <-> IA)
+    printf("----------- Language Options -------------------\n");
+    printf("0. Translate from English to Igpay Atinlayn\n");
+    printf("1. Translate from Igpay Atinlay to English\n");
+    printf("----------------------------------------------------\n");
+    printf("\nEnter your desired translation direction (0 or 1): ");
 }
