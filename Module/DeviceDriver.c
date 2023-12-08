8da369767b6111606794905396ed5f576d845d1b
Sidney <sid.14boys@yahoo.fr>
2023-12-08 14:29:37 -0800

    Add readFromDevice function to handle reading data from the device

--- Module/DeviceDriver.c
+++ Module/DeviceDriver.c
@@ -49,3 +49,53 @@ static int openDevice(struct inode *inode, struct file *fs)
     printk(KERN_INFO "Device opened!\n");
     return 0;
 }
+
+// read data from the device
+static ssize_t readFromDevice(struct file *fs, char __user *buf, size_t hsize, loff_t *off)
+{
+    char test[BUFFER_SIZE];
+    int end = 3;
+
+    int j;
+    if ((test[0] == 'a' || test[0] == 'e' || test[0] == 'i' || test[0] == 'o' || test[0] == 'u' ||
+         test[0] == 'A' || test[0] == 'E' || test[0] == 'I' || test[0] == 'O' || test[0] == 'U') &&
+        test[end - 3] == 'y')
+    {
+        // translation logic for words starting with vowels and ending with 'y'
+        for (j = 0; j < BUFFER_SIZE - 2; ++j)
+        {
+            // translation: Move first letter to end and add "ay"
+            char firstLetter = test[0];
+            for (int i = 0; i < BUFFER_SIZE - 2; ++i)
+            {
+                test[i] = test[i + 1];
+            }
+            test[BUFFER_SIZE - 2] = firstLetter;
+            test[BUFFER_SIZE - 1] = 'a';
+            test[BUFFER_SIZE] = 'y';
+            test[BUFFER_SIZE + 1] = '\0';
+            break;
+        }
+        printk(KERN_INFO "Translated: %s", test);
+    }
+    else
+    {
+        // translation logic for words not starting with vowels or not ending with 'y'
+        int len = strlen(test);
+        for (int i = 0; i < len / 2; ++i)
+        {
+            char temp = test[i];
+            test[i] = test[len - i - 1];
+            test[len - i - 1] = temp;
+        }
+        printk(KERN_INFO "Reversed: %s", test);
+    }
+
+    if (copy_to_user(buf, test, hsize))
+    {
+        printk(KERN_ERR "Cannot read data.\n");
+        return -EFAULT;
+    }
+    printk(KERN_INFO "Data read : %s", buf);
+    return hsize;
+}
